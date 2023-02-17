7cd6e58deca75aca8f1ac090f31bf8a06c36915d
Artsiom_PH <workartpharm@gmail.com>
2023-02-17 19:20:35 +0300

    solved replace task

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,16 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        max_item = 0
+
+        for item in input_list:
+            if item > max_item:
+                max_item = item
+
+        for index, item in enumerate(input_list):
+            if item > 0:
+                input_list[index] = max_item
+        return input_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
